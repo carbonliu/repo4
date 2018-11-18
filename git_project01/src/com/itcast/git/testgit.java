064fa0291677098cd170be63a70bf9ecd2922993
liuk <18860401360@163.com>
2018-11-18 14:45:28 +0800

    test.java

---
+++ git_project01/src/com/itcast/git/testgit.java
@@ -0,0 +1,17 @@
+package com.itcast.git;
+
+/**
+ * @author LiuK
+ * @date 2018/11/18 14:41
+ * @description:
+ */
+public class testgit {
+    public static void main(String[] args) {
+        System.out.println("1111111111111111111");
+        System.out.println("1111111111111111111");
+        System.out.println("2222222222222222222");
+        System.out.println("2222222222222222222");
+        System.out.println("3333333333333333333");
+        System.out.println("3333333333333333333");
+    }
+}
